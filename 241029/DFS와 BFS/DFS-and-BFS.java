5b0862648a604bd9325d908a208a4f01747dd042
JeongHyeon Jo <99077276+CISXO@users.noreply.github.com>
2024-10-29 14:45:58 +0900

    Title: DFS와 BFS, Time: 312ms, Memory: 18MB, Status: Passed - CodeTree

--- 241029/DFS와 BFS/DFS-and-BFS.java
+++ 241029/DFS와 BFS/DFS-and-BFS.java
@@ -1,10 +1,7 @@
-package codetest.bfs_dfs;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
