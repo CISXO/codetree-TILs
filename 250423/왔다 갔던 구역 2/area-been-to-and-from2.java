b74f11b94384fb8c71658ead24282940a5699900
JeongHyeon Jo <99077276+CISXO@users.noreply.github.com>
2025-04-23 19:29:35 +0900

    Title: 왔다 갔던 구역 2, Time: 103ms, Memory: 13MB, Status: Passed - Codetree

--- 250423/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250423/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -10,6 +10,8 @@ public class Main {
 
     static ArrayList<Integer> list = new ArrayList<>();
 
+    static ArrayList<Integer> contact = new ArrayList<>();
+
     public static void main(String[] args) throws IOException{
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -18,28 +20,46 @@ public class Main {
         N = Integer.parseInt(bf.readLine());
 
         int dir = 0;
+        String current ="x";
 
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(bf.readLine());
             int k = Integer.parseInt(st.nextToken());
             String data = st.nextToken();
+            
             if(data.equals("L")) {
                 for(int j=0; j<k; j++) {
+                    if(current.equals("R")) {
+                        dir++;
+                    }
+
                     dir--;
                     que.add(dir);
+                    current="L";
                 }
             } else if(data.equals("R")) {
                 for(int j=0; j<k; j++) {
+                    if(current.equals("L")) {
+                        dir--;
+                    }
+
                     dir++;
                     que.add(dir);
+
+                    current="R";
                 }
             }
         }
 
         for(int data : que) {
+            //System.out.print(data + " ");
             if(!list.contains(data)) {
                 list.add(data);
             } else {
+                if(contact.contains(data)) {
+                    continue;
+                }
+                contact.add(data);
                 result++;
             }
         }
